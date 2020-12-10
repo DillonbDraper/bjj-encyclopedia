d406834299bf4c18b9022e542f12186830ae3652
Dillon Draper <dillonbdraper@gmail.com>
2020-12-10 11:59:44 -0600

    Fixed duplicate in database and got dropdowns to both render and communicate appropriately.  All dropdowns are capable of broadcasting relevant information through state objects

--- src/components/dropdowns/DropdownList.js
+++ src/components/dropdowns/DropdownList.js
@@ -1,24 +1,16 @@
 import react, {useContext, useState, useEffect} from 'react'
 import { PositionContext } from './PositionProvider'
-import { SubpositionContext } from './SubpositionProvider'
-import { OrientationContext } from './OrientationProvider'
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import { OrientandSubDropdown, OrientandSubDropwdown } from "./OrientandSubDropdown"
 
 export const DropdownList = props => {
 
     const { positions, getPositions } = useContext(PositionContext)
-    const { orientations, getOrientations } = useContext(OrientationContext)
-    const { subpositions, getSubpositions } = useContext(SubpositionContext)
 
-    useEffect(() => getSubpositions().then(getOrientations).then(getPositions), [])
-    
-    const [ positionValue, setpositionValue ] = useState("")
-    const [ orientationValue, setOrientationValue ] = useState("")
-    const [ subpositionValue, setSubpositionValue ] = useState("")
+    useEffect(() => getPositions(), [])
 
-    console.log(orientations)
-    console.log(positionValue)
+    const [ positionValue, setpositionValue ] = useState({})
 
     return (
         <div className="dropdowns">
@@ -31,28 +23,7 @@ export const DropdownList = props => {
               onChange={(event, newValue) => setpositionValue(newValue)}
               renderInput={(params) => <TextField {...params} label="Position" variant="outlined" />}
             />
-            <Autocomplete
-              id="orientation"
-              options={orientations}
-              getOptionLabel={(orient) => {
-                  if (orient.dominant === true && (positionValue.id === 1 || 2 ||)) {
-                    return "Top"
-                  } else {return "Bottom"}
-              }}
-              style={{ width: 300 }}
-              value={orientationValue}
-              onChange={(event, newValue) => setOrientationValue(newValue)}
-              renderInput={(params) => <TextField {...params} label="Orientation" variant="outlined" />}
-            />
-            <Autocomplete
-              id="subpositions"
-              options={subpositions}
-              getOptionLabel={(subposish) => subposish.name}
-              style={{ width: 300 }}
-              value={subpositionValue}
-              onChange={(event, newValue) => setsubpositionValue(newValue)}
-              renderInput={(params) => <TextField {...params} label="Position" variant="outlined" />}
-            />
+            {positionValue !== "" ? <OrientandSubDropdown key={1} positionValue={positionValue} /> : ""}
         </div>
 
           );
---
+++ src/components/dropdowns/OrientandSubDropdown.js
@@ -0,0 +1,58 @@
+import react, { useContext, useState, useEffect } from 'react'
+import { SubpositionContext } from './SubpositionProvider'
+import { OrientationContext } from './OrientationProvider'
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+export const OrientandSubDropdown = ({ positionValue }) => {
+    const { orientations, getOrientations } = useContext(OrientationContext)
+    const { subpositions, getSubpositions } = useContext(SubpositionContext)
+
+    useEffect(() => getSubpositions().then(getOrientations), [])
+
+    const [ orientationValue, setOrientationValue ] = useState({})
+    const [ subpositionValue, setsubpositionValue ] = useState({})
+
+    let filteredSubposition = []
+    if (positionValue === null) {
+        filteredSubposition = []
+    } else {
+    filteredSubposition = subpositions.filter(sub => sub.positionId === positionValue.id)
+    }
+
+    return (
+        <>
+            <Autocomplete
+                id="orientation"
+                disabled={positionValue.id === 3 ? true : false}
+                options={orientations}
+                getOptionLabel={(orient) => {
+                    console.log(orient)
+                    if (orient.dominant === true && (positionValue.id === 1 || positionValue.id === 2 || positionValue.id === 4 || positionValue.id === 5 || positionValue.id === 7 || positionValue.id === 9)) {
+                        return "Top"
+                    } else if (orient.dominant === true && (positionValue.id === 6 || positionValue.id === 8)) {
+                        return "Superior"
+                    } else if (orient.dominant === false && (positionValue.id === 1 || positionValue.id === 2 || positionValue.id === 4 || positionValue.id === 5 || positionValue.id === 7 || positionValue.id === 9)) {
+                        return "Bottom"
+                    } else if (orient.dominant === false && (positionValue.id === 6 || positionValue.id === 8)) {
+                        return "Inferior"
+                    }
+                }}
+                style={{ width: 300 }}
+                value={orientationValue}
+                onChange={(event, newValue) => setOrientationValue(newValue)}
+                renderInput={(params) => <TextField {...params} label="Orientation" variant="outlined" />}
+            />
+            <Autocomplete
+                id="subpositions"
+                options={filteredSubposition}
+                getOptionLabel={(subposish) => subposish.name}
+                style={{ width: 300 }}
+                value={subpositionValue}
+                onChange={(event, newValue) => setsubpositionValue(newValue)}
+                renderInput={(params) => <TextField {...params} label="Subposition" variant="outlined" />}
+            />
+        </>
+    )
+
+}
