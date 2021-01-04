dc2d798818a3953b848d9b38e354619d8796d096
Dillon Draper <dillonbdraper@gmail.com>
2021-01-03 19:38:51 -0600

    Further styling changes.  Logo cropped and centered nicely, now blends better with page.  If no further progress is made, this is acceptable for demo.  Minor changes still desired

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,9 @@
 import React from "react"
-import logo from "../images/default.png"
+import logo from "../images/bjj-logo.png"
 import "./logo.css"
 
 export const Logo = () => (
+    <div className="logoContainer">
     <img className="logo" src={logo} alt="Can't load logo" />
+    </div>
 )
